4be0a5329f6159f63889a7ed89a4ae92827e2d27
Jeff Puckett <jeff@jeffpuckett.com>
2017-04-28 22:39:08 +0000

    add vue js

--- js/comments-github-issue.js
+++ js/comments-github-issue.js
@@ -6,20 +6,15 @@ $(document).ready(function () {
         headers: {Accept: "application/vnd.github.v3.html+json"},
         dataType: "json",
         success: function(comments) {
-            $.each(comments, function(i, comment) {
-                var t  = '<div class="panel panel-default">';
-                    t += '<div class="panel-heading">';
-                    t += '<img src="' + comment.user.avatar_url + '" width="24px">';
-                    t += ' <strong><a href="' + comment.user.html_url + '">' + comment.user.login + '</a></strong>';
-                    t += ' ' + comment.created_at;
-                    t += '</div>';
-                    t += '<div class="panel-body">' + comment.body_html + '</div>';
-                    t += '</div>';
-                $("#gh-comments-list").append(t);
+            new Vue({
+                el: '#gh-comments-list',
+                data: {
+                    comments: comments
+                }
             });
         },
         error: function() {
-            $("#gh-comments-list").append("Comments are not open for this post yet.");
+            $("#gh-comments").hide();
         }
     });
 });
